968ae964d81db4b8c71f4cc835cdc603f98113f1
Marcus Andersson <marcus@apparant.se>
2023-02-11 12:47:38 +0100

    Skapat projekt och Controller

---
+++ CdSamling/Models/CdModel.cs
@@ -0,0 +1,10 @@
+namespace CdSamling.Models {
+    public class CdSkivan {
+        //Properties
+        public int Id { get; set; }
+        public string? CdName { get; set; }
+        public string? Artist { get; set; }
+        public int? NrTracks { get; set; }
+        public int? ReleaseYear { get; set; }
+    }
+}
---
+++ CdSamling/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CdSamling.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
